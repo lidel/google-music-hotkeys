fe7796496b72be804a7feb6c04c4313acf057d67
Marcin Rataj <lidel@lidel.org>
2017-10-10 20:33:26 +0200

    Batman!
    (this commit has no parents)

---
+++ README.md
@@ -0,0 +1,39 @@
+# Google Music Hotkeys
+
+> Browser extension that adds keyboard shortcuts for controlling Google Music
+
+## Background
+
+I wanted to control background playback of Google Music without switching tabs.
+That is all.
+
+## Features
+
+- no GUI ;-)
+- works in Firefox and Chrome
+- playback control
+    - toggle playback: <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>8</kbd>
+    - previous song: <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>9</kbd>
+    - next song: <kbd>Ctrl</kbd>+<kbd>Shift</kbd>+<kbd>0</kbd>
+- customize default shortcuts
+    - Chrome: via `chrome://extensions/configureCommands` dialog
+    - Firefox: no cigar yet (https://bugzilla.mozilla.org/show_bug.cgi?id=1303384)
+
+## Install
+
+TODO
+
+## Development
+
+```
+npm install
+npm run build
+npm run firefox
+```
+
+## License
+
+`Play_music_triangle.png` from https://commons.wikimedia.org/wiki/File:Play_music_triangle.svg
+
+The add-on itself is released under [CC0](LICENSE): to the extent possible under law, the author has waived all copyright and related or neighboring rights to this work, effectively placing it in the public domain.
+
---
+++ add-on/background.js
@@ -0,0 +1,52 @@
+'use strict'
+/* eslint-env browser, webextensions */
+
+const googleMusicPlayerUrl = 'https://play.google.com/music/listen*'
+
+function scriptFor (command) {
+  switch (command) {
+    case 'toggle-playback':
+      return scriptThatClicksOn('play-pause')
+    case 'previous-song':
+      return scriptThatClicksOn('rewind')
+    case 'next-song':
+      return scriptThatClicksOn('forward')
+  }
+}
+
+function scriptThatClicksOn (actionName) {
+  let script = function () {
+    let buttons = document.getElementsByTagName('paper-icon-button')
+    for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i].getAttribute('data-id') === 'kitty') {
+        buttons[i].click()
+        break
+      }
+    }
+  }
+  return '(' + script.toString().replace('kitty', actionName) + ')()'
+}
+
+browser.commands.onCommand.addListener(async (command) => {
+  console.log('[Google Music Hotkeys] executing command: ', command)
+  const gmTabs = await browser.tabs.query({
+    url: googleMusicPlayerUrl,
+    status: 'complete'
+  })
+  for (let tab of gmTabs) {
+    // console.log('tab', tab)
+    browser.tabs.executeScript(tab.id, {
+      code: scriptFor(command)
+    })
+  }
+})
+
+/*
+let gettingAllCommands = browser.commands.getAll()
+gettingAllCommands.then((commands) => {
+  for (let command of commands) {
+    console.log(command)
+  }
+})
+*/
+
